0b77b01a56e14e60f149396c51007155bc65b9e1
zidanpahrudin_ <zidanpahrudin4@gmail.com>
2020-09-19 15:05:50 +0800

    added new fetures

--- src/awesome.js
+++ src/awesome.js
@@ -1,6 +1,6 @@
 export default {
   sauce: function(){
     alert('your excellent website is now been awesome sauced');
-    console.log('your excellent website website is now awesome');
+    console.log('zidan');
   }
 }
